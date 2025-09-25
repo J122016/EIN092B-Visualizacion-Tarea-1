a320250f9d6ebcc46313d6dbb84d128d9e8cc8a1
chthonautics <>
2025-09-25 15:41:55 -0300

    refactoring pt 1
    renombrado dataset_analysis para ser mas descriptivo, actualizado .gitignore

---
+++ dataset_analysis.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+def graph_entidades_titulados_anno(
+    df: pd.DataFrame, export: str, display: bool = True
+) -> None:
+    plt.figure(figsize=(10, 6))
+    plot = sns.countplot(data=df, x="AÑO", palette="viridis")
+    plt.title("Número de entidades (filas) con titulados por año")
+    plt.xlabel("Año")
+    plot.set_xticklabels(df["AÑO"].dt.year.unique())
+    plt.ylabel("Número de entidades (filas) con titulados")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    if display:
+        plt.show()
+    plot.figure.savefig(export, dpi=300)
+    plt.close()
+    return
+
+
+def graph_total_titulados_anno(
+    df: pd.DataFrame, export: str, display: bool = True
+) -> pd.DataFrame:
+    plt.figure(figsize=(10, 6))
+    df["TOTAL TITULACIONES ACUM"] = df.groupby("AÑO")["TOTAL TITULACIONES"].cumsum()
+    plot = sns.lineplot(
+        data=df, x="AÑO", y="TOTAL TITULACIONES ACUM", marker="o", color="b"
+    )
+    plt.title("Total de titulados por año")
+    plt.xlabel("Año")
+    plt.ylabel("Total de titulados")
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    if display:
+        plt.show()
+    plot.figure.savefig(export, dpi=300)
+    plt.close()
+    return df
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from dataset_pipeline import load_dataset
-from dataset_analysis_test import test_entidades_titulados_anno, test_total_titulados_anno
+from dataset_analysis import *
 
 
 def main():
-    FILENAME = 'TITULADO_2007-2024_web_19_05_2025_E.csv'
+    FILENAME = "TITULADO_2007-2024_web_19_05_2025_E.csv"
     EXPORT_PATH = "exports/"
     EXPORT_COUNTER = 0
 
@@ -14,21 +14,26 @@ def main():
     print("Cargando dataset...")
     df = load_dataset(FILENAME)
     df.info()
-    with pd.option_context('display.max_columns', 42):
-        print(df.describe(include='all'))
+    with pd.option_context("display.max_columns", 42):
+        print(df.describe(include="all"))
     print(df.head(5))
     print("Dataset cargado.")
 
     # Gráficos
     # 1. Grafico de prueba - entidad con titulados (filas) en cada año
-    test_entidades_titulados_anno(df, export=f"{EXPORT_PATH}_{EXPORT_COUNTER}_test_analysis.svg", display=True)
+    graph_entidades_titulados_anno(
+        df, export=f"{EXPORT_PATH}_{EXPORT_COUNTER}_test_analysis.svg", display=True
+    )
     EXPORT_COUNTER += 1
 
     # 1. Grafico de prueba - titulados por año
-    test_total_titulados_anno(df, export=f"{EXPORT_PATH}_{EXPORT_COUNTER}_test_analysis.svg", display=True)
+    graph_total_titulados_anno(
+        df, export=f"{EXPORT_PATH}_{EXPORT_COUNTER}_test_analysis.svg", display=True
+    )
     EXPORT_COUNTER += 1
 
     return
+
 
 if __name__ == "__main__":
     main()
